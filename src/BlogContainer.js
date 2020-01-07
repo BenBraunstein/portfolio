1fcc34c11e7b7583a2ec11a24816938d35b0e1a1
Ben Braunstein <benzbraunstein@gmail.com>
2020-01-07 14:20:41 -0500

    put all blog posts in a grid

--- src/BlogContainer.js
+++ src/BlogContainer.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import { Container } from '@material-ui/core';
-import BlogPost from './BlogPost';
-
-
+import React from "react"
+import { Grid } from "@material-ui/core"
+import BlogPost from "./BlogPost"
 
 function BlogContainer() {
-
-    return <div>
-        <h2>Blog Posts</h2>
-        <Container maxWidth="md">
-            <BlogPost image="https://miro.medium.com/max/498/0*eRCRV3XiHNVub60u" title="The Journey from Windows to macOS and back to Windows…" href="https://medium.com/@benzbraunstein/the-journey-from-windows-to-macos-and-back-to-windows-12f6311ca424" description="Since I was a young child, I was blessed to have the opportunity of having personal computers in my home. I loved everything about them. Using them, fixing them… and sometimes breaking them. The PCs in my home started with Windows XP, next up was Vista and eventually Windows 7 (my parents never wanted to upgrade to Windows 10)..." />
-            <BlogPost image="https://miro.medium.com/max/632/0*mvGwNsYnxuiQAwjz.gif" title="How to use React Hooks to give functional components their own 'State'" href="https://medium.com/@benzbraunstein/how-to-use-react-hooks-to-give-functional-components-their-own-state-ade4c756244f" description="State in React is something truly awesome. It gives you the ability to create components which have the ability to change their values during the lifetime, whereas a components props never change. Once you give a component a prop, that prop is static and cannot be changed unless the component gets removed or re-rendered. Although state is awesome, it is only accessible to use in components that are Class Based Ones, rather than in Functional Components, however React added “Hooks” a way Functional Components can change their value..." />
-            <BlogPost image="https://miro.medium.com/max/1168/1*lJuK3pJiMt5g1-AJfRPpsg.png" title="How to build a simple chat application using JavaScript and the Rails API" href="https://medium.com/@benzbraunstein/how-to-build-a-simple-chat-application-using-javascript-and-the-rails-api-68c2577db9ea" description="Ever wondered how difficult it is to create a basic chat application by yourself (please just play along) ? I wondered the exact same thing once I dove headfirst into JavaScript and began to ponder all the awesome projects I could build using the awesome language. I decided to give this project a go and see how difficult it would be for me to build…and in about an hour and a half my basic application was complete..." />
-            <BlogPost image="https://s3.amazonaws.com/law-media/uploads/131/51997/large/man_suit_mask_impersonation_ss.jpg?1525714001" title="Be afraid. I can impersonate you" href="https://medium.com/@benzbraunstein/be-weary-i-can-impersonate-you-adb51ede042a" description="Ah. email. The internet has many historical firsts that will forever be remembered. The first documented use of emoticons, The first domain name was registered in 1985, The first website about the World Wide Web went live in 1991, The first picture was uploaded to the internet in 1992, The first item was sold on eBay before it was even called eBay, The first YouTube video was posted in 2005, The first tweet was sent in 2006. Before all of these “firsts” the first email was sent in 1971 by Ray Tomlinson..." />
-        </Container>
-
+  return (
+    <div>
+      <h2>Blog Posts</h2>
+      <Grid style={{ padding: "30px" }} container spacing={3}>
+        <Grid item xs>
+          <BlogPost
+            image="https://miro.medium.com/max/498/0*eRCRV3XiHNVub60u"
+            title="The Journey from Windows to macOS and back to Windows…"
+            href="https://medium.com/@benzbraunstein/the-journey-from-windows-to-macos-and-back-to-windows-12f6311ca424"
+            description="Since I was a young child, I was blessed to have the opportunity of having personal computers in my home. I loved everything about them. Using them, fixing them… and sometimes breaking them. The PCs in my home started with Windows XP, next up was Vista and eventually Windows 7 (my parents never wanted to upgrade to Windows 10)..."
+          />
+        </Grid>
+        <Grid item xs>
+          <BlogPost
+            image="https://miro.medium.com/max/632/0*mvGwNsYnxuiQAwjz.gif"
+            title="How to use React Hooks to give functional components their own 'State'"
+            href="https://medium.com/@benzbraunstein/how-to-use-react-hooks-to-give-functional-components-their-own-state-ade4c756244f"
+            description="State in React is something truly awesome. It gives you the ability to create components which have the ability to change their values during the lifetime, whereas a components props never change. Once you give a component a prop, that prop is static and cannot be changed unless the component gets removed or re-rendered. Although state is awesome, it is only accessible to use in components that are Class Based Ones, rather than in Functional Components, however React added “Hooks” a way Functional Components can change their value..."
+          />
+        </Grid>
+        <Grid item xs>
+          <BlogPost
+            image="https://miro.medium.com/max/1168/1*lJuK3pJiMt5g1-AJfRPpsg.png"
+            title="How to build a simple chat application using JavaScript and the Rails API"
+            href="https://medium.com/@benzbraunstein/how-to-build-a-simple-chat-application-using-javascript-and-the-rails-api-68c2577db9ea"
+            description="Ever wondered how difficult it is to create a basic chat application by yourself (please just play along) ? I wondered the exact same thing once I dove headfirst into JavaScript and began to ponder all the awesome projects I could build using the awesome language. I decided to give this project a go and see how difficult it would be for me to build…and in about an hour and a half my basic application was complete..."
+          />
+        </Grid>
+        <Grid item xs>
+          <BlogPost
+            image="https://s3.amazonaws.com/law-media/uploads/131/51997/large/man_suit_mask_impersonation_ss.jpg?1525714001"
+            title="Be afraid. I can impersonate you"
+            href="https://medium.com/@benzbraunstein/be-weary-i-can-impersonate-you-adb51ede042a"
+            description="Ah. email. The internet has many historical firsts that will forever be remembered. The first documented use of emoticons, The first domain name was registered in 1985, The first website about the World Wide Web went live in 1991, The first picture was uploaded to the internet in 1992, The first item was sold on eBay before it was even called eBay, The first YouTube video was posted in 2005, The first tweet was sent in 2006. Before all of these “firsts” the first email was sent in 1971 by Ray Tomlinson..."
+          />
+        </Grid>
+      </Grid>
     </div>
-
-
+  )
 }
 
 export default BlogContainer
--- src/Header.js
+++ src/Header.js
@@ -1,94 +1,91 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer'
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import MenuIcon from '@material-ui/icons/Menu'
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Drawer from "@material-ui/core/Drawer"
+import Button from "@material-ui/core/Button"
+import List from "@material-ui/core/List"
+import Divider from "@material-ui/core/Divider"
+import ListItem from "@material-ui/core/ListItem"
+import MenuIcon from "@material-ui/icons/Menu"
 // import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import ListItemText from "@material-ui/core/ListItemText"
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core"
 // import InboxIcon from '@material-ui/icons/MoveToInbox';
 // import MailIcon from '@material-ui/icons/Mail';
 
 const useStyles = makeStyles({
-    list: {
-        width: 250,
-    },
-    fullList: {
-        width: 'auto',
-    },
-    root: {
-        flexGrow: 1,
-    },
-    title: {
-        flexGrow: 1,
-    },
-});
-
+  list: {
+    width: 250
+  },
+  fullList: {
+    width: "auto"
+  },
+  root: {
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+})
 
 function Header() {
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false
+  })
 
-    const classes = useStyles();
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+  const toggleDrawer = (side, open) => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return
+    }
 
-    const toggleDrawer = (side, open) => event => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
+    setState({ ...state, [side]: open })
+  }
 
-        setState({ ...state, [side]: open });
-    };
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        <ListItem button>Home</ListItem>
+        <ListItem button>Projects</ListItem>
+        <ListItem button>Blog Posts</ListItem>
+        <ListItem button>Contact</ListItem>
+      </List>
+    </div>
+  )
 
-    const sideList = side => (
-        <div
-            className={classes.list}
-            role="presentation"
-            onClick={toggleDrawer(side, false)}
-            onKeyDown={toggleDrawer(side, false)}
-        >
-            <List>
-                <ListItem button >
-                    Home
-                </ListItem>
-                <ListItem button >
-                    Projects
-                </ListItem>
-                <ListItem button >
-                    Blog Posts
-                </ListItem>
-                <ListItem button >
-                    Contact
-                </ListItem>
-            </List>
-        </div>
-    );
-
-    return (
-        <div>
-            <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
-                {sideList('left')}
-            </Drawer>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer('left', true)} >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Ben Braunstein
-                    </Typography>
-
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div>
+      <Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+        {sideList("left")}
+      </Drawer>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleDrawer("left", true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Ben Braunstein
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
-
 
 export default Header
