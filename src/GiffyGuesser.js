8caf319d39079d8495d32dfe5afa689304092b4c
Ben Braunstein <BenZBraunstein@gmail.com>
2020-01-23 23:31:15 -0500

    added giffy guesser

--- src/GiffyGuesser.js
+++ src/GiffyGuesser.js
@@ -43,7 +43,7 @@ const useStyles = makeStyles(theme => ({
   }
 }))
 
-function Tradr() {
+function GiffyGuesser() {
   const classes = useStyles()
   const [imageModal, changeImageModal] = useState(false)
   const [currentImage, changeCurrentImage] = useState({})
@@ -60,74 +60,59 @@ function Tradr() {
     {
       title: "Homepage",
       url:
-        "https://camo.githubusercontent.com/7be3493dce65651d501979080183e413e6569666/68747470733a2f2f692e696d6775722e636f6d2f53434e4275394a2e706e67"
+        "https://camo.githubusercontent.com/cc0e604a598085aad3d6077e157045e161e301ae/68747470733a2f2f692e696d6775722e636f6d2f6d4363424f5a5a2e706e67"
     },
     {
-      title: "New Item",
+      title: "Matching Game Question",
       url:
-        "https://camo.githubusercontent.com/7a4cb5ba5362d631da2cc5ff4fd20c4c1823bdf1/68747470733a2f2f692e696d6775722e636f6d2f596b64394458622e706e67"
+        "https://camo.githubusercontent.com/be259047b69025141440ceab0c878c269512d98c/68747470733a2f2f692e696d6775722e636f6d2f546d4b586e6b452e706e67"
     },
     {
-      title: "Outgoing Trade Notification",
+      title: "Incorrectly Answered Movie Question",
       url:
-        "https://camo.githubusercontent.com/805dc6cbff42b7c82d5b8868732f448caa36cdf5/68747470733a2f2f692e696d6775722e636f6d2f6f4636466432582e706e67"
-    },
-    {
-      title: "Incoming Trade Notification",
-      url:
-        "https://camo.githubusercontent.com/3cb4bb8b961910426e60b4b3d5abaf69e9f0edb7/68747470733a2f2f692e696d6775722e636f6d2f4e33446e7362622e706e67"
-    },
-    {
-      title: "Tradr Chat Window",
-      url:
-        "https://camo.githubusercontent.com/555a8ab634f6ca318a2c6ea4f1e8bec5e8261a30/68747470733a2f2f692e696d6775722e636f6d2f574662303058762e706e67"
-    },
-    {
-      title: "SMS Notification",
-      url:
-        "https://camo.githubusercontent.com/0435a5e77aecdd9dc2eb30c0c57a02b0abddbac3/68747470733a2f2f692e696d6775722e636f6d2f5955397176446b2e706e67"
+        "https://camo.githubusercontent.com/354a1ac43df9e67fae92794d935c4ec4bb1809de/68747470733a2f2f692e696d6775722e636f6d2f33434e397043752e706e67"
     }
   ]
 
   return (
     <div style={{ width: "70%", margin: "auto" }}>
-      <h1>Tradr</h1>
+      <h1>Giffy Guesser</h1>
       <h2>Motivation</h2>
       <p>
-        Everyone has old stuff they don't use anymore, which sits in the corner
-        of your room or in the back of a closet somewhere. You don't want to get
-        rid of these because they have value. Either that value is to you or to
-        someone else, it still exists. Tradr is an app where you could finally
-        get these things out of your house (and make your spouse happy) and into
-        the hands of someone who will finally put them to good use once again.
-        Not only will you rid yourself of these items, you can get something of
-        actual value in return.
+        Everyone loves a good gif, but have you ever played a multiple choice
+        game where you try and pick specific gifts? Thats exactly why we made
+        one.
+        <br />
+        We built an application which automatically generates questions for our
+        four game modes:
+        <ul>
+          <li>
+            The Synonym Game - Try and guess the search term that found this Gif
+          </li>
+          <li>The Dog Breed Game - Try and guess the dog breed in this Gif</li>
+          <li>The Movie Game - Try and Guess the movie this Gif comes from</li>
+          <li>
+            The Matching Game - Given a searched term and 4 Gifs. Try and guess
+            the least popular Gif for from this search term
+          </li>
+        </ul>
       </p>
       <h2>Frameworks</h2>
       <p>
-        This app is built using React.js, FileStack, Rails 5 API and the Twilio
-        API to send SMS messages.
+        This app is created with the Rails MVC Framework and uses the Tenor API
+        to find Gifs.
       </p>
-      <h2>Features</h2>
-      <ul>
-        <li>
-          Authentication and Authorization using Bcrypt and JSON Web Tokens.
-        </li>
-        <li>
-          Add items you want to trade into your list of items (and add images of
-          them using FilesStack)
-        </li>
-        <li>View, search and filter all available items for trade</li>
-        <li>Propose trades with others.</li>
-        <li>Integrated notification system to see all pending trades.</li>
-        <li>
-          New trades use Twilio API to send SMS messages and notify recipient of
-          the trade.
-        </li>
-        <li>Users of the application can chat using Text and Emoji.</li>
-      </ul>
+      <h2>How to use:</h2>
+      <p>
+        Sign up or log in to an existing account. Click "Choose a Game", then
+        pick the type of game you want to play. You can also challenege another
+        user who has recently played and play the exact same game as they did to
+        try and beat their score and get your name to the top of the
+        leaderboards for that game. In your profile you can see your completed
+        games as well as your overall statistics in all game types.
+      </p>
       <h2>Screenshots</h2>
-      <Container key="Tradr" className={classes.root}>
+      <Container key="GiffyGuesser" className={classes.root}>
         {/* Here is the image scroller */}
         <GridList className={classes.gridList} cols={2.5}>
           {tileData.map(tile => (
@@ -176,19 +161,13 @@ function Tradr() {
         </Fade>
       </Modal>
       <h2>
-        <a href="https://github.com/BenBraunstein/tradr-front">
-          Front-End Github
-        </a>{" "}
-        |{" "}
-        <a href="https://github.com/MildlyConfused/tradr-backend">
-          Back-End Github
-        </a>
+        <a href="https://github.com/BenBraunstein/GifGame">Github Page</a>
       </h2>
       <h2>
-        <a href="https://tradr-frontend.herokuapp.com/">Try it on Heroku!</a>
+        <a href="https://giffyguesser.herokuapp.com/">Try it on Heroku!</a>
       </h2>
     </div>
   )
 }
 
-export default Tradr
+export default GiffyGuesser
--- src/ProjectContainer.js
+++ src/ProjectContainer.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Tradr from "./Tradr"
+import GiffyGuesser from "./GiffyGuesser"
 
 function ProjectContainer() {
   return (
     <div className="tradr">
-      <Tradr />
+      <GiffyGuesser />
     </div>
   )
 }
