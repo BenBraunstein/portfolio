b7c37d7b588e21e18bc1d4eb117e61fd07280318
Ben Braunstein <benzbraunstein@gmail.com>
2020-01-23 21:25:05 -0500

    added some info for tradr

--- src/Tradr.js
+++ src/Tradr.js
@@ -67,20 +67,58 @@ function Tradr() {
   ]
 
   return (
-    <Container key="Tradr" className={classes.root}>
+    <div>
       <h1>Tradr</h1>
-      <GridList className={classes.gridList} cols={2.5}>
-        {tileData.map(tile => (
-          <GridListTile key={tile.img}>
-            <img src={tile.url} alt={tile.title} />
-            <GridListTileBar
-              title={tile.title}
-              classes={{ root: classes.titleBar, title: classes.title }}
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </Container>
+      <h2>Motivation</h2>
+      <p>
+        Everyone has old stuff they don't use anymore, which sits in the corner
+        of your room or in the back of a closet somewhere. You don't want to get
+        rid of these because they have value. Either that value is to you or to
+        someone else, it still exists. Tradr is an app where you could finally
+        get these things out of your house (and make your spouse happy) and into
+        the hands of someone who will finally put them to good use once again.
+        Not only will you rid yourself of these items, you can get something of
+        actual value in return.
+      </p>
+      <h2>Frameworks</h2>
+      <p>
+        This app is built using React.js, FileStack, Rails 5 API and the Twilio
+        API to send SMS messages.
+      </p>
+      <h2>Features</h2>
+      <ul>
+        <li>
+          Authentication and Authorization using Bcrypt and JSON Web Tokens.
+        </li>
+        <li>
+          Add items you want to trade into your list of items (and add images of
+          them using FilesStack)
+        </li>
+        <li>View, search and filter all available items for trade</li>
+        <li>Propose trades with others.</li>
+        <li>Integrated notification system to see all pending trades.</li>
+        <li>
+          New trades use Twilio API to send SMS messages and notify recipient of
+          the trade.
+        </li>
+        <li>Users of the application can chat using Text and Emoji.</li>
+      </ul>
+      <h2>Screenshots</h2>
+      <Container key="Tradr" className={classes.root}>
+        {/* Here is the image scroller */}
+        <GridList className={classes.gridList} cols={2.5}>
+          {tileData.map(tile => (
+            <GridListTile key={tile.img}>
+              <img src={tile.url} alt={tile.title} />
+              <GridListTileBar
+                title={tile.title}
+                classes={{ root: classes.titleBar, title: classes.title }}
+              />
+            </GridListTile>
+          ))}
+        </GridList>
+      </Container>
+    </div>
   )
 }
 
