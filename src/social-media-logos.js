a0e19887eee04a57832eeb0e079ff56e9752762b
Ben Braunstein <benzbraunstein@gmail.com>
2020-01-28 14:31:46 -0500

    added whatsapp click to text and adjusted subject for email click

--- src/social-media-logos.js
+++ src/social-media-logos.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react"
 
 function SocialButtons() {
-    return <div className='SocialButtons'>
+  // prettier-ignore
+  return <div className='SocialButtons'>
         <a href="https://github.com/BenBraunstein" className="csmb-github csmb-round csmb">Github: BenBraunstein</a>
         <a href="https://medium.com/@benzbraunstein" className="csmb-medium csmb-round csmb">Medium: @BenZBraunstein</a>
         <a href="https://www.linkedin.com/in/ben-braunstein/" className="csmb-linkedin csmb-round csmb">LinkedIn: Ben-Braunstein</a>
         <a href="https://www.facebook.com/Ben22Braunstein" className="csmb-facebook csmb-round csmb">Facebook: Ben22Braunstein</a>
         <a href="https://www.youtube.com/channel/UCHsE7n9LugIfrsfzPae9E3Q" className="csmb-youtube csmb-round csmb">Youtube: Ben Braunstein</a>
-        <a href="mailto:BenZBraunstein@gmail.com" className="csmb-mail csmb-round csmb">Email: BenZBraunstein@gmail.com</a>
+        <a href="https://wa.me/15164778178?text=Hi%20Ben%2C%20I%20love%20your%20website%21" className="csmb-whatsapp csmb-round csmb">Whatsapp: +1 516 477 8178</a>
+        <a href="mailto:BenZBraunstein@gmail.com?subject=Hi From Your Portfolio Site" className="csmb-mail csmb-round csmb">Email: BenZBraunstein@gmail.com</a>
     </div>
 }
 
