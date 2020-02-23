8e9fa8e68220e310c5b3664cc0864c05b0e211a8
Ben Braunstein <benzbraunstein@gmail.com>
2020-02-23 09:05:38 -0500

    added the custom cursor and made it disappear when it leaves the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Header from "./Header"
 import BlogContainer from "./BlogContainer"
 import { Route, Switch, withRouter } from "react-router-dom"
@@ -9,31 +9,39 @@ import Footer from "./Footer"
 import Tradr from "./Tradr"
 import GiffyGuesser from "./GiffyGuesser"
 import Home from "./Home"
-// import Cursor from "./Cursor"
+import Cursor from "./Cursor"
 
 function App(props) {
-  // const [xcord, changex] = useState(0)
-  // const [ycord, changey] = useState(0)
-  // const [cursorClasses, changeClasses] = useState(["cursor"])
+  const [xcord, changex] = useState(0)
+  const [ycord, changey] = useState(0)
+  const [cursorClasses, changeClasses] = useState(["cursor"])
 
   return (
     <div
       className="App"
-      // onMouseMove={e => {
-      //   changex(e.pageX + "px")
-      //   changey(e.pageY + "px")
-      // }}
-      // onMouseDown={() => {
-      //   changeClasses([...cursorClasses, "mouse-down"])
-      // }}
-      // onMouseUp={() => {
-      //   changeClasses(
-      //     cursorClasses.filter(className => className !== "mouse-down")
-      //   )
-      // }}
+      onMouseMove={e => {
+        changex(e.pageX + "px")
+        changey(e.pageY + "px")
+      }}
+      onMouseDown={() => {
+        changeClasses([...cursorClasses, "mouse-down"])
+      }}
+      onMouseUp={() => {
+        changeClasses(
+          cursorClasses.filter(className => className !== "mouse-down")
+        )
+      }}
+      onMouseLeave={() => {
+        changeClasses([...cursorClasses, "mouse-left"])
+      }}
+      onMouseEnter={() => {
+        changeClasses(
+          cursorClasses.filter(className => className !== "mouse-left")
+        )
+      }}
     >
       <Header history={props.history} />
-      {/* <Cursor classes={cursorClasses} xcord={xcord} ycord={ycord} /> */}
+      <Cursor classes={cursorClasses} xcord={xcord} ycord={ycord} />
       <Switch>
         <Route path="/blogs" render={() => <BlogContainer />} />
         <Route path="/projects/tradr" render={() => <Tradr />} />
