46c2cff095e2ed7f4d1c83e1a45822a9c7f45940
Ben Braunstein <benzbraunstein@gmail.com>
2020-03-20 15:23:08 -0400

    made download button actually work

--- src/Resume.js
+++ src/Resume.js
@@ -13,8 +13,13 @@ function Resume() {
         style={{ backgroundColor: "#1976D2", color: "white" }}
         size="large"
         startIcon={<SaveIcon />}
+        onClick={() =>
+          window.open(
+            "https://drive.google.com/u/0/uc?id=15_cMKtKWm9QdD4kX1ZCY4QBC_Rxd9QzL&export=download"
+          )
+        }
       >
-        Download
+        Download PDF
       </Button>
     </div>
   )
