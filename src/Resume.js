4ded5710e73b52bfffd04edef1d7b1272ee63cf1
Ben Braunstein <benzbraunstein@gmail.com>
2020-03-20 15:19:48 -0400

    added a download button

--- src/Resume.js
+++ src/Resume.js
@@ -1,10 +1,21 @@
 import React from "react"
 import reactJpg from "./images/resume.jpg"
+import { Button } from "@material-ui/core"
+import SaveIcon from "@material-ui/icons/Save"
 
 function Resume() {
   return (
     <div>
       <img src={reactJpg} alt="Resume" width="1000px" />
+      <br />
+      <Button
+        variant="contain"
+        style={{ backgroundColor: "#1976D2", color: "white" }}
+        size="large"
+        startIcon={<SaveIcon />}
+      >
+        Download
+      </Button>
     </div>
   )
 }
